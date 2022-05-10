e522f99427c14780ecb4ce458d900b8ccb6b6602
Alejandro <alex.galez2218@gmail.com>
2022-05-10 14:01:06 +0200

    creación componentes not-found y nuevo-negocio

--- frontend/src/app/nuevo-negocio/negocio/negocio.component.ts
+++ frontend/src/app/nuevo-negocio/negocio/negocio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-negocio',
@@ -13,3 +13,65 @@ export class NegocioComponent implements OnInit {
   }
 
 }
+
+function escribirTextoArchivo() {
+  console.log("Paso por función 1");
+  alert("¡Enhorabuena! ¡Has registrado el negocio!");
+}
+
+function escribirTextoArchivo2() {
+  alert("¡Limpiaste TODOS los campos!");
+}
+
+function saludoBob() {
+  alert("¡Hola! ¡Me llamo Bob!")
+}
+
+//document.querySelector('botonRegistro2').addEventListener('click', (e:Event) => escribirTextoArchivo2());
+
+//let selectorFormularioFarmacia = document.getElementById('formFarmacia');
+
+//let selectorFormularioFarmacia: any = document.querySelector('#formFarmacia');
+// getElementById("formFarmacia") as HTMLInputElement | null;
+//let selectorFormularioFarmacia = new ElementRef('formFarmacia');
+//let variableFormulario = new ElementRef('eleccionAsociacion');
+
+//console.info(variableFormulario);
+//console.warn(selectorFormularioFarmacia);
+//console.info(selectorFormularioFarmacia);
+
+
+//var selectorTipoNegocio: any = document.getElementById('tipoNegocio2')! as HTMLInputElement | null;
+/*let selectorTipoNegocio: any = document.querySelector('#tipoNegocio2');
+selectorTipoNegocio.addEventListener(('select'), function () {
+    elegirNegocio();
+});*/
+
+
+/*
+selectorTipoNegocio.addEventListener(('change'), function () {
+    var opcionSeleccionada: any = this.options[selectorTipoNegocio.selectedIndex];
+    elegirNegocio();
+});*/
+
+/*
+let selectorOptica: any = document.getElementById('selectorOptica');
+let selectorFarmacia: any = document.getElementById('selectorFarmacia');
+
+
+function elegirNegocio() {
+    if (selectorTipoNegocio.value == "farmacia") {
+        //selectorOptica.remove();
+        selectorOptica.setAttribute('hidden','');
+        selectorFarmacia.removeAttribute('hidden');
+        console.warn('¡Has borrado la parte de la optica!');
+    } if (selectorTipoNegocio.value == "optica") {
+        //selectorFarmacia.remove();
+        selectorFarmacia.setAttribute('hidden','');
+        selectorOptica.removeAttribute('hidden');
+        console.warn('¡Has borrado la parte de la farmacia!');
+    } if (selectorTipoNegocio.value == "---") {
+        selectorFarmacia.setAttribute('hidden','');
+        selectorOptica.setAttribute('hidden','');
+    }
+}*/
