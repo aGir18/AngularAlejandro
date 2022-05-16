f63a620deebbf2d42f360672e0e765429174f175
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-05-16 13:39:52 +0100

    pequeños cambios

--- frontend/src/app/negocios-asociados/creacion-negocios/creacion-negocios.component.ts
+++ frontend/src/app/negocios-asociados/creacion-negocios/creacion-negocios.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faPencil, faEye, faTrashCan, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { Negocio } from '../models/negocio';
 
 @Component({
   selector: 'app-creacion-negocios',
@@ -7,11 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreacionNegociosComponent implements OnInit {
 
-  public nuevoNegocio = {tipoNegocio: '', nombreNegocio: '', nifNegocio: '', puntosSigre: 0, autorefractometros:0};
+  public nuevoNegocio = {
+    tipoNegocio: '',
+    nombreNegocio: '',
+    nifNegocio: '', puntosSigre: 0,
+    autorefractometros:0,
+    asociacion:''
+  };
+  public negocios: Negocio[] = [];
+  public numeroNegocios = 0;
+
+  public guardarNegocio() {
+    this.negocios.push({...this.nuevoNegocio});
+    this.numeroNegocios = this.negocios.length;
+  }
+
+  public borrarNegocio(negocio: Negocio){
+    this.negocios = this.negocios.filter(c => c.nombreNegocio !== negocio.nombreNegocio );
+    this.numeroNegocios = this.negocios.length;
+  }
+
+  public asociaciones = [
+    { id: 1, nombre: 'Félix González García'},
+    { id: 2, nombre: 'Alberto Núñez Morcillo'},
+    { id: 3, nombre: 'Antonio Pisón Saravia'},
+    { id: 4, nombre: 'Eduardo Ríos Sasiaín'},
+  ];
+
+  public mostrarNegocios(){
+    this.negocios.forEach(element => {
+      element.nombreNegocio
+    });
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  pencil=faPencil;
+  eye=faEye;
+  trash=faTrashCan;
+  plus=faCirclePlus;
 
 }
---
+++ frontend/src/app/negocios-asociados/models/negocio.ts
@@ -0,0 +1,8 @@
+export interface Negocio {
+  tipoNegocio: string;
+  nombreNegocio: string;
+  nifNegocio: string;
+  puntosSigre: number;
+  autorefractometros: number;
+  asociacion: string;
+}
